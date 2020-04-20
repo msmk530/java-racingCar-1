c4ecfc712ef2471208af6f238e21ad3d3ea86c4f
kimminsub <msmk530@naver.com>
2020-04-20 21:48:01 +0900

    test : Car 생성자 테스트

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -24,7 +24,7 @@ public class Car {
         }
     }
 
-    public boolean isCarMove(int random) {
+    boolean isCarMove(int random) {
         if(random>=POSSIBLE_NUMBER_FOR_MOVE){
             return true;
         }
--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,22 +1,42 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
 
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("sub");
+    }
+
+    @Test
+    void 생성자_테스트() {
+        List<Car> testCar = new LinkedList<>();
+        testCar.add(new Car("min"));
+        testCar.add(new Car("kim"));
+
+        assertThat(testCar).extracting("name").contains("kim");
+        assertThat(testCar).extracting("name").contains("sub");
+    }
+
     @Test
     void 랜덤숫자에따른_이동여부_테스트(){
-        Car car = new Car("sub");
         assertThat(car.isCarMove(3)).isTrue();
     }
 
     @Test
     void 랜덤숫자에따른_포지션_업데이트_테스트() {
-        Car car = new Car("sub");
-        car.updatePosition(4);
+        Car positionTestCar = new Car("min");
+        positionTestCar.updatePosition(4);
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
