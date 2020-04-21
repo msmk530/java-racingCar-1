a54d9ec6cb516d2c6f2fce707c7acc267dd98f93
kimminsub <msmk530@naver.com>
2020-04-21 15:47:00 +0900

    test : 중복이름 테스트

--- src/test/java/domain/ValidatorTest.java
+++ src/test/java/domain/ValidatorTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,4 +19,9 @@ class ValidatorTest {
         assertThat(Validator.isValidRoundNumber(-1)).isTrue();
     }
 
+    @Test
+    void 중복_이름_테스트() {
+        String[] testNames = {"kim","min","sub","sub"};
+        assertThat(Validator.isDuplicatedName(testNames)).isTrue();
+    }
 }
