0f1364a1dff0bea32f29314d7d951e6c51a657ca
kimminsub <msmk530@naver.com>
2020-04-21 15:25:56 +0900

    test : 라운드진행 테스트

--- src/test/java/domain/GameProgressTest.java
+++ src/test/java/domain/GameProgressTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,20 +8,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GameProgressTest {
 
+    private Car[] cars;
+
+    @BeforeEach
+    void setUp() {
+        String[] allCarOfName = {"kim  ", "min  ", "sub  "};
+        cars = GameProgress.makeCar(allCarOfName);
+
+    }
+
     @Test
     void 자동차_만들기_테스트() {
-        String[] allCarOfName = {"kim  ","min  ","sub  "};
-        Car[] cars = GameProgress.makeCar(allCarOfName);
-
         assertThat(cars[0].getName()).isEqualTo("kim");
         assertThat(cars[1].getName()).isEqualTo("min");
         assertThat(cars[2].getName()).isEqualTo("sub");
-
     }
 
     @Test
     void 라운드_진행_테스트() {
+        boolean updateFlag = false;
 
+        for (Car car : cars) {
+            assertThat(car.getPosition()).isEqualTo(0);
+        }
 
+        GameProgress.progressRound(cars);
+        for (Car car : cars) {
+          if(car.getPosition()!=0){
+              updateFlag = true;
+              break;
+          }
+        }
+        assertThat(updateFlag).isEqualTo(true);
     }
+
 }
