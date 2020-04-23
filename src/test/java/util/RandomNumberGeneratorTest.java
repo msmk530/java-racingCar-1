bf2f36889b6a688044321e837b49623aa963d404
kimminsub <msmk530@naver.com>
2020-04-23 12:19:07 +0900

    refactor : isBetween사용

--- src/test/java/util/RandomNumberGeneratorTest.java
+++ src/test/java/util/RandomNumberGeneratorTest.java
@@ -8,6 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class RandomNumberGeneratorTest {
     @Test
     void 랜덤숫자_범위_테스트() {
-        assertThat(RandomNumberGenerator.generateRandomNumber()).isLessThan(10);
+        assertThat(RandomNumberGenerator.generateRandomNumber()).isBetween(0,9);
     }
 }
